3d9e7cf9308b3c6a71fe727de52975866bf3d00b
Nayeem <nayeemuddin.bd100@gmail.com>
2021-07-13 03:24:03 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home/Home';
-import NotFound from './components/NotFound/NotFound';
 
 function App() {
     return (
         <div className="App">
-            <Switch>
-                <Route path="/home">
-                    <Home />
-                </Route>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-                <Route path="*">
-                    <NotFound />
-                </Route>
-            </Switch>
+            <h2>Testingggg</h2>
+            <h2>Heloo world!!</h2>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
-// Ant Desgine CDN
-// import 'antd/dist/antd.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-        ,
+        <App />,
     </React.StrictMode>,
     document.getElementById('root')
 );
